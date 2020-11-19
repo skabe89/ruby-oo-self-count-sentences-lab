fbb3978ec07eca6210a01ccb4fa9bb73be68e846
Sean <skabe89@gmail.com>
2020-11-19 12:57:57 -0500

    commit tree 642b1717c86644bb85c0e407c75b0ee06c97f3f0

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    count = []
+    array = self.split(/[.?!]/)
+    array != nil ? array.each{|i| count << i if i.size > 2} : nil
+    count.size
   end
 end
